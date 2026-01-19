810028500c888ee8cf4638400a6f78b0f69a9b99
camilo-zuluaga <zuluagajuanca@outlook.es>
2026-01-19 18:13:01 -0500

    feat(refactor): client func init

--- zotero/zotero_client.go
+++ zotero/zotero_client.go
@@ -1,10 +1,7 @@
 package zotero
 
 import (
-	"encoding/json"
-	"fmt"
 	"net/http"
-	"net/url"
 )
 
 type ZoteroClient struct {
@@ -14,39 +11,10 @@ type ZoteroClient struct {
 	Client  *http.Client
 }
 
-func (z *ZoteroClient) FetchAllItems() ([]ZoteroItem, error) {
-	url := z.buildURL("items")
-	return fetch[ZoteroItem](z, url)
-}
-
-func (z *ZoteroClient) FetchItemsByCategory(collectionKey string) ([]ZoteroItem, error) {
-	url := z.buildURL("collections", collectionKey, "items")
-	return fetch[ZoteroItem](z, url)
-}
-
-func (z *ZoteroClient) FetchCollections() ([]Collection, error) {
-	url := z.buildURL("collections")
-	return fetch[Collection](z, url)
-}
-
-func fetch[T any](c *ZoteroClient, url string) ([]T, error) {
-	res, err := c.Client.Get(url)
-	if err != nil {
-		return nil, fmt.Errorf("failed to fetch: %w", err)
+func NewZoteroClient(baseURL, userID string) *ZoteroClient {
+	return &ZoteroClient{
+		BaseURL: baseURL,
+		UserID:  userID,
+		Client:  &http.Client{},
 	}
-	defer res.Body.Close()
-
-	var items []T
-	if err := json.NewDecoder(res.Body).Decode(&items); err != nil {
-		return nil, fmt.Errorf("failed to decode response: %w", err)
-	}
-
-	return items, nil
-}
-
-func (z *ZoteroClient) buildURL(segments ...string) string {
-	u, _ := url.Parse(z.BaseURL)
-	pathParts := append([]string{"users", z.UserID}, segments...)
-	u.JoinPath(pathParts...)
-	return u.String()
 }
