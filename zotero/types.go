4502387c628a333954afc58b07eb0e15f3373255
camilo-zuluaga <zuluagajuanca@outlook.es>
2026-01-19 18:04:14 -0500

    feat: types.go file

--- zotero/types.go
+++ zotero/types.go
@@ -11,3 +11,15 @@ type ZoteroItemData struct {
 	Title       string
 	Collections []string
 }
+
+type Collection struct {
+	Key  string
+	Meta struct {
+		NumItems       int
+		NumCollections int
+	}
+	Data struct {
+		Name    string
+		Version int
+	}
+}
