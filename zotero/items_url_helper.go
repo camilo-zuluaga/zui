b8a68e146df99c969c10670b47f7013af8b8deb5
camilo-zuluaga <zuluagajuanca@outlook.es>
2026-01-21 20:57:52 -0500

    feat: url builder for items query (pagination)

---
+++ zotero/items_url_helper.go
@@ -0,0 +1,51 @@
+package zotero
+
+import (
+	"fmt"
+	"net/url"
+	"path"
+	"strconv"
+)
+
+var baseURL = "https://api.zotero.org"
+
+type ItemsQuery struct {
+	CollectionKey string
+	Q             string
+	QMode         string
+	Start         int
+	Limit         int
+}
+
+func buildItemsURL(userID string, opts ItemsQuery) (string, error) {
+	u, err := url.Parse(baseURL)
+	if err != nil {
+		return "", fmt.Errorf("invalid base URL: %w", err)
+	}
+
+	var p string
+	if opts.CollectionKey != "" {
+		p = fmt.Sprintf("/users/%s/collections/%s/items",
+			url.PathEscape(userID), url.PathEscape(opts.CollectionKey))
+	} else {
+		p = fmt.Sprintf("/users/%s/items", url.PathEscape(userID))
+	}
+	u.Path = path.Join(u.Path, p)
+
+	q := url.Values{}
+	if opts.Q != "" {
+		q.Set("q", opts.Q)
+	}
+	if opts.QMode != "" {
+		q.Set("qmode", opts.QMode)
+	}
+	if opts.Start >= 0 {
+		q.Set("start", strconv.Itoa(opts.Start))
+	}
+	if opts.Limit > 0 {
+		q.Set("limit", strconv.Itoa(opts.Limit))
+	}
+
+	u.RawQuery = q.Encode()
+	return u.String(), nil
+}
