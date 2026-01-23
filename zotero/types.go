812270e20ec34ddc4cfe5742942db9d0743aeb5e
camilo-zuluaga <zuluagajuanca@outlook.es>
2026-01-22 20:58:57 -0500

    feat: new zotero types and json typed

--- zotero/types.go
+++ zotero/types.go
@@ -1,25 +1,37 @@
 package zotero
 
 type ZoteroItem struct {
-	Key  string
-	Data ZoteroItemData
+	Key  string         `json:"key"`
+	Data ZoteroItemData `json:"data"`
 }
 
 type ZoteroItemData struct {
-	Key         string
-	ItemType    string
-	Title       string
-	Collections []string
+	ItemType    string              `json:"itemType"`
+	Title       string              `json:"title"`
+	Date        string              `json:"date"`
+	NumPages    string              `json:"numPages"`
+	Creators    []ZoteroItemCreator `json:"creators"`
+	Collections []string            `json:"collections"`
+}
+
+type ZoteroItemCreator struct {
+	CreatorType string `json:"creatorType"`
+	FirstName   string `json:"firstName"`
+	LastName    string `json:"lastName"`
+}
+
+type Meta struct {
+	NumItems       int `json:"numItems,omitempty"`
+	NumCollections int `json:"numCollections,omitempty"`
+}
+
+type Data struct {
+	Name    string `json:"name,omitempty"`
+	Version int    `json:"version,omitempty"`
 }
 
 type Collection struct {
-	Key  string
-	Meta struct {
-		NumItems       int
-		NumCollections int
-	}
-	Data struct {
-		Name    string
-		Version int
-	}
+	Key  string `json:"key"`
+	Meta Meta
+	Data Data
 }
