7cc2021c0ce2ab60b3b4c9493dac953a2b695048
camilo-zuluaga <zuluagajuanca@outlook.es>
2026-01-22 20:24:39 -0500

    feat: pagination functionality for fetching

--- zotero/fetch.go
+++ zotero/fetch.go
@@ -7,30 +7,55 @@ import (
 	"fmt"
 	"net/http"
 	"net/url"
+	"strings"
 )
 
 func fetch[T any](c *ZoteroClient, ctx context.Context, url string) ([]T, error) {
-	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create request: %w", err)
-	}
-	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", c.ApiKey))
+	var allItems []T
+	currentURL := url
 
-	res, err := c.Client.Do(req)
-	if err != nil {
-		if errors.Is(err, context.Canceled) || errors.Is(err, context.DeadlineExceeded) {
+	for currentURL != "" {
+		req, err := http.NewRequestWithContext(ctx, http.MethodGet, currentURL, nil)
+		if err != nil {
+			return nil, fmt.Errorf("failed to create request: %w", err)
+		}
+		req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", c.ApiKey))
+
+		res, err := c.Client.Do(req)
+		if err != nil {
+			if errors.Is(err, context.Canceled) || errors.Is(err, context.DeadlineExceeded) {
+				return nil, err
+			}
 			return nil, err
 		}
-		return nil, err
-	}
-	defer res.Body.Close()
 
-	var items []T
-	if err := json.NewDecoder(res.Body).Decode(&items); err != nil {
-		return nil, fmt.Errorf("failed to decode response: %w", err)
+		var items []T
+		if err := json.NewDecoder(res.Body).Decode(&items); err != nil {
+			res.Body.Close()
+			return nil, fmt.Errorf("failed to decode response: %w", err)
+		}
+
+		allItems = append(allItems, items...)
+		currentURL = parseNextURL(&res.Header)
+		res.Body.Close()
 	}
 
-	return items, nil
+	return allItems, nil
+}
+
+func parseNextURL(h *http.Header) string {
+	link := h.Get("Link")
+	if link == "" {
+		return ""
+	}
+	// the structure of the zotero response is the following:
+	// link: <https://api.zotero.org/users/19402717/collections/IXWDFSNI/items?limit=40&start=40>; rel="next", ...
+	// so I'm assuming the first < and first > will contain the url for the next set of items
+	if strings.Contains(link, `rel="next"`) {
+		firstAnchor, lastAnchor := strings.Index(link, "<"), strings.Index(link, ">")
+		return link[firstAnchor+1 : lastAnchor]
+	}
+	return ""
 }
 
 func (z *ZoteroClient) buildURL(segments ...string) string {
