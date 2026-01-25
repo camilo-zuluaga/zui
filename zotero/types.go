c7d0eb0c5f93f34b108f4df1880c9a99f09cf22e
camilo-zuluaga <zuluagajuanca@outlook.es>
2026-01-24 20:27:10 -0500

    feat: zotero general item

--- zotero/types.go
+++ zotero/types.go
@@ -1,5 +1,20 @@
 package zotero
 
+// General Zotero item trying to deal with heterogeneous data
+type ZoteroGeneralItem struct {
+	Key  string `json:"key"`
+	Data struct {
+		ItemType    string              `json:"itemType"`
+		ParentItem  string              `json:"parentItem,omitempty"`
+		Title       string              `json:"title"`
+		URL         string              `json:"url,omitempty"`
+		Filename    string              `json:"filename,omitempty"`
+		Date        string              `json:"date"`
+		Creators    []ZoteroItemCreator `json:"creators,omitempty"`
+		Collections []string            `json:"collections,omitempty"`
+	}
+}
+
 type ZoteroItem struct {
 	Key  string         `json:"key"`
 	Data ZoteroItemData `json:"data"`
@@ -11,13 +26,21 @@ type ZoteroItemData struct {
 	Date        string              `json:"date"`
 	NumPages    string              `json:"numPages"`
 	Creators    []ZoteroItemCreator `json:"creators"`
-	Collections []string            `json:"collections"`
+	Attachment  ZoteroAttachment
+	Collections []string `json:"collections"`
 }
 
 type ZoteroItemCreator struct {
 	CreatorType string `json:"creatorType"`
 	FirstName   string `json:"firstName"`
 	LastName    string `json:"lastName"`
+}
+
+type ZoteroAttachment struct {
+	Key      string
+	Title    string
+	Filename string
+	URL      string
 }
 
 type Meta struct {
