800c0dfcdcfdffc5a0744f5545b24887c285d688
camilo-zuluaga <zuluagajuanca@outlook.es>
2026-01-28 18:04:27 -0500

    feat: search item method

--- zotero/api_methods.go
+++ zotero/api_methods.go
@@ -1,6 +1,8 @@
 package zotero
 
-import "context"
+import (
+	"context"
+)
 
 func (z *ZoteroClient) FetchAllItems(ctx context.Context) ([]ZoteroItem, error) {
 	url := z.buildURL("items")
@@ -17,10 +19,23 @@ func (z *ZoteroClient) FetchItemsByCollection(ctx context.Context, collectionKey
 	if err != nil {
 		return nil, err
 	}
-	return GroupItemsByParent(res), nil
+	return GroupItems(res), nil
 }
 
 func (z *ZoteroClient) FetchCollections(ctx context.Context) ([]Collection, error) {
 	url := z.buildURL("collections")
 	return fetch[Collection](z, ctx, url)
+}
+
+func (z *ZoteroClient) SearchItem(ctx context.Context, query string) ([]ZoteroItem, error) {
+	url, _ := buildItemsURL(z.BaseURL, z.UserID, ItemsQuery{
+		Q:     query,
+		Start: 0,
+		Limit: 50,
+	})
+	res, err := fetch[ZoteroGeneralItem](z, ctx, url)
+	if err != nil {
+		return nil, err
+	}
+	return GroupItems(res), nil
 }
